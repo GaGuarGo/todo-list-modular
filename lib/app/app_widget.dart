23cba1e58f30558fa07b6c9ffdefa295aa581b03
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-25 11:12:40 -0300

    Configurando Factory de Conexão com sqlite

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -6,9 +6,12 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Todo List Modular',
-      home: SplashPage(),
+      home: const SplashPage(),
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
     );
   }
 }
