b68a2b40629b459001ccec236a07df4ada07c40b
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-25 10:51:54 -0300

    Configuração Inicial do Projeto

---
+++ android/app/src/main/kotlin/br/com/gaguargo/todo_list_modular/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.gaguargo.todo_list_modular
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_modular/app/app_module.dart';
+
+void main() {
+  runApp(const AppModule());
+}
