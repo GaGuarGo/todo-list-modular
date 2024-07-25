2bd7b0373406888d35140b76cedee5bfababa017
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-25 16:14:12 -0300

    Configurando Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list_modular/app/app_module.dart';
+import 'package:todo_list_modular/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppModule());
 }
